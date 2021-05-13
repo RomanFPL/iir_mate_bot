8c1f0d92abe037650a84aef5e195a10ae734ccb5
Roman <roma.franko99@gmail.com>
2021-05-13 19:18:07 +0300

    first start configuration

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const TOKEN = "1801326232:AAFyzR7-s2C76zQbbt8RMJaihgggN_LLk8U";
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf('1801326232:AAFyzR7-s2C76zQbbt8RMJaihgggN_LLk8U');
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
